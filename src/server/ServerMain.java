9102c6194a8aca16d7b9d359d51a5782e4171c0c
U-2MUCHPRN\Carl <dat14cda@student.lu.se>
2016-12-04 18:03:07 +0100

    http added, but now operating at faulty ports

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -5,16 +5,21 @@ import java.net.UnknownHostException;
 
 public class ServerMain {
 
-		public static void main(String[] args) throws UnknownHostException, IOException {
-			Camera[] camera = new Camera[2];
-			for (int i = 0; i < 2; i++) {
-				String server = null;
-				int port = 8011+ i;
-				camera[i] = new Camera(server, port);
-			}
-			for (Camera cam : camera) {
-				cam.start();
-			}
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Camera[] camera = new Camera[2];
+		JPEGHTTPServer[] HTTP = new JPEGHTTPServer[2];
+		for (int i = 0; i < 2; i++) {
+			String server = null;
+			int port = 8011 + i;
+			camera[i] = new Camera(server, port);
+			HTTP[i] = new JPEGHTTPServer(port+2);
 		}
-
+		for (Camera cam : camera) {
+			cam.start();
+		}
+		for (JPEGHTTPServer s: HTTP) {
+			s.start();
+		}
 	}
+
+}
